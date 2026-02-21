494b3db1874bdcff4521d1ca98d746521aec4d22
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-02-21 17:49:40 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
+  base: "/retro-arcade-ai/",
+
+  build: {
+    outDir: "dist",
+    assetsDir: "assets"
+  },
+
   server: {
-    port: 3000
+    port: 3000,
+    open: true
   }
 });
