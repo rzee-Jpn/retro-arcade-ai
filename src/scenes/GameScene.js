46c0163de8075b6d8c922b9e24bbfcfcd4b23ab2
rzee-Jpn <64503323+rzee-Jpn@users.noreply.github.com>
2026-02-21 15:17:54 +0800

    Update GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -5,14 +5,33 @@ export default class GameScene extends Phaser.Scene {
     super("GameScene");
   }
 
-  preload() {}
-
   create() {
-    this.add.text(100, 300, "AI Arcade Starter", {
-      fontSize: "20px",
-      fill: "#ffffff"
-    });
-  }
+    this.physics.world.setBounds(0, 0, 360, 640);
 
-  update() {}
+    this.platforms = this.physics.add.staticGroup();
+
+    // Ground
+    this.platforms.create(180, 620, null)
+      .setDisplaySize(360, 40)
+      .refreshBody();
+
+    // Middle platforms
+    this.platforms.create(180, 480, null)
+      .setDisplaySize(300, 20)
+      .refreshBody();
+
+    this.platforms.create(180, 340, null)
+      .setDisplaySize(250, 20)
+      .refreshBody();
+
+    this.platforms.create(180, 200, null)
+      .setDisplaySize(200, 20)
+      .refreshBody();
+
+    // Simple graphics for platforms
+    this.add.rectangle(180, 620, 360, 40, 0x4444ff);
+    this.add.rectangle(180, 480, 300, 20, 0x4444ff);
+    this.add.rectangle(180, 340, 250, 20, 0x4444ff);
+    this.add.rectangle(180, 200, 200, 20, 0x4444ff);
+  }
 }
